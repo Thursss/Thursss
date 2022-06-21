2652781e6b2c95b019d716666e3d92fe6faa5134
zhousiping <1720557879@qq.com>
2022-06-21 14:12:08 +0800

    bureau-of-statistics init

---
+++ project/bureau-of-statistics/src/views/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>统计局</div>
+  <section>
+    <router-link to="/">home</router-link>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class HomeView extends Vue {}
+</script>
